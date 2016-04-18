77f5ac2dc97730ce0d07ab1f064c3a720abc6684
Allison Mann <anmx85@mail.missouri.edu>
2016-04-18 13:39:47 -0500

    removed bad javascript

--- fangirl/jquery.js
+++ fangirl/jquery.js
@@ -12,34 +12,3 @@ $(document).ready(function() {
 	 }
 	});//end resize
 });//end ready
-//ON RESIZE
-$(window).resize(function () {clearTimeout(body.stories);this.class = stories(checkTimer, 500);});
-function checkTimer() {
-    var width = $("stories").width();
-//MOBILE
-    if (width < 640) {
-        mobileView();
-    }
-//TABLET
-    else if (width > 640 && width <966) {
-        appendFix();
-    }
-//DESKTOP
-    else if (width >966) {
-        appendFix();
-    }
-};
-//ON RELOAD
-    var width2 = $(window).width();
-//MOBILE
-    if (width2 < 640) {
-        mobileView();
-    };
-//TABLET
-    if (width2 > 640 && width2 <966) {
-        appendFix();
-    };
-//DESKTOP
-    if (width2 > 966) {
-        appendFix();
-    };
