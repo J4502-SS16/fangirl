cf2a149891555c7e5ef02b1bc8d14e04e840713f
Kylee May <kjmhv3@mail.missouri.edu>
2016-04-19 11:48:49 -0500

    styling/jquery/html nav bar wii u

--- fangirl/jquery.js
+++ fangirl/jquery.js
@@ -36,38 +36,38 @@ window.onclick = function(event) {
 }
 
 function myFunction() {
-    document.getElementById("myDropdown1").classList.toggle("show");
+    document.getElementById("myDropdown1").classList.toggle("show1");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.dropbtn1')) {
 
     var dropdowns = document.getElementsByClassName("dropdown-content1");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains('show1')) {
+        openDropdown.classList.remove('show1');
       }
     }
   }
 }
 
 function myFunction() {
-    document.getElementById("myDropdown2").classList.toggle("show");
+    document.getElementById("myDropdown2").classList.toggle("show2");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.dropbtn2')) {
 
     var dropdowns = document.getElementsByClassName("dropdown-content2");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains('show2')) {
+        openDropdown.classList.remove('show2');
       }
     }
   }
