1bb39e6b70832b30fe9ed1158aa703bf27fd4eaf
Rob Weir <weirr@missouri.edu>
2016-04-19 09:45:07 -0500

    Added dropdown menu jQuery back
    to file

--- fangirl/jquery.js
+++ fangirl/jquery.js
@@ -12,3 +12,25 @@ $(document).ready(function() {
 	 }
 	});//end resize
 });//end ready
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
